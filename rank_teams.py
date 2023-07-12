ce8eb7cc6a0ad2e9e65d2e2519f120291b55ed9a
evihuijben <e.m.c.huijben@tue.nl>
2023-07-12 18:54:24 +0200

    cleaner code and save to csv

--- rank_teams.py
+++ rank_teams.py
@@ -19,7 +19,6 @@ def create_export_dict(csv_file):
                             export_dict[submission] = {}
                             for title, value in zip(titles, row):
                                 export_dict[submission][title] = value
-                                
                         else: 
                             warnings.warn('Not unique ID')
     metrics_dict = {}
@@ -56,8 +55,8 @@ def define_best_and_worst(metrics_dict, aggregate='mean'):
     return best, worst
 
 
-def normalize_metrics(metrics_dict):
-    best, worst = define_best_and_worst(metrics_dict)
+def normalize_metrics(metrics_dict, aggregate='mean'):
+    best, worst = define_best_and_worst(metrics_dict, aggregate)
     
     normalized = {}
     for i, (submission, metrics) in enumerate(metrics_dict.items()):
@@ -68,27 +67,40 @@ def normalize_metrics(metrics_dict):
             if best[metric] == worst[metric]:
                 normalized[submission][metric] = 0.5
             else:
-                normalized[submission][metric] = (values['mean'] - worst[metric]) / (best[metric] - worst[metric])
+                normalized[submission][metric] = (values[aggregate] - worst[metric]) / (best[metric] - worst[metric])
         normalized[submission]['sum'] = sum(normalized[submission].values())
     return normalized
 
     
-def rank(normalized_results, metrics_dict, export_dict):
+def rank_and_save(normalized_results, metrics_dict, export_dict, aggregate='mean'):
     sorted_results = (sorted(normalized_results.items(), key=lambda x:x[1]['sum'], reverse=True))
-    print('Rank \tTeam \t\t\t\tSummed normalized metrics')
-    for team_rank, res in enumerate(sorted_results):
-        team = export_dict[res[0]]['title']
-        team = team[team.find(' ')+1:]
-        print(f"{team_rank:04d} \t{team:20}\t{res[1]['sum']:.2f}")
     
+    
+    with open('final_ranking.csv', 'w') as outf:
+        writer = csv.writer(outf)
+        print('Rank \tTeam \t\t\t\tSummed normalized metrics')
+        for team_rank, (submission, result) in enumerate(sorted_results):
+            team = export_dict[submission]['title']
+            team = team[team.find(' ')+1:]
+            print(f"{team_rank+1:04d} \t{team:20}\t{result['sum']:.2f}")
+            
+            save_row = [team_rank+1, team, submission, result['sum']]
+            if team_rank == 0:
+                header = ['Rank','team', 'pk','Sum of normalized metrics']
+            for metric in metrics_dict[submission].keys():
+                save_row.extend([metrics_dict[submission][metric][aggregate], result[metric]])
+                if team_rank == 0:
+                    header.extend([f"{metric} (absolute)", f"{metric} (normalized)"])                
+            if team_rank == 0:
+                writer.writerow(header)
+            writer.writerow(save_row)
 
     
 if __name__=="__main__":
     leader_board_export = 'leaderboard_export.csv'
     export_dict, metrics_dict = create_export_dict(leader_board_export)
-    
     normalized_results = normalize_metrics(metrics_dict)
-    rank(normalized_results, metrics_dict, export_dict)
+    rank_and_save(normalized_results, metrics_dict, export_dict)
     
     
     
